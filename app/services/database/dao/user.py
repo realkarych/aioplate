53be528e59cce49460d3f1b12003bb0a3c065b75
yakarych <kaarych@gmail.com>
2023-09-09 11:15:38 +0300

    Remove generic dao as design antipattern

--- app/services/database/dao/user.py
+++ app/services/database/dao/user.py
@@ -1,15 +1,13 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.dtos.user import UserDTO
-from app.models.user import User
-from app.services.database.dao.base import BaseDAO
 
 
-class UserDAO(BaseDAO[User]):
+class UserDAO:
     """ORM queries for users table"""
 
     def __init__(self, session: AsyncSession):
-        super().__init__(User, session)
+        self._session = session
 
     async def add_user(self, user: UserDTO) -> None:
         await self._session.merge(user.to_db_model())
